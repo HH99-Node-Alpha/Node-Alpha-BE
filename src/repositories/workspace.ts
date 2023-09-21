aaa828f9f6034a8264dbef634e9ffa6d4461c5b2
whdgnszz1 <whdgnszz1@snu.ac.kr>
2023-09-21 16:07:28 +0900

    Feat: 워크스페이스를 생성할 때 기본적으로 1개의 board가 생성되게하는 로직 추가

--- src/repositories/workspace.ts
+++ src/repositories/workspace.ts
@@ -1,11 +1,13 @@
 import prisma from '../utils/prisma';
+import BoardsRepository from './boards';
 
 class WorkspacesRepository {
-  createWorkspace = async (workspaceName: string, ownerId: number) => {
+  constructor(private readonly boardsRepository: BoardsRepository) {}
+  async createWorkspace(workspaceName: string, ownerId: number) {
     const workspace = await prisma.workspaces.create({
       data: {
-        workspaceName: workspaceName,
-        ownerId: ownerId,
+        workspaceName,
+        ownerId,
         WorkspacesMembers: {
           create: {
             UserId: ownerId,
@@ -14,8 +16,29 @@ class WorkspacesRepository {
       },
     });
 
+    const existingBoards = await prisma.boards.findMany({
+      where: {
+        WorkspaceId: workspace.workspaceId,
+      },
+    });
+
+    if (existingBoards.length === 0) {
+      // 워크스페이스에 연결된 보드가 없을 경우
+      const user = await prisma.users.findUnique({
+        where: { userId: ownerId },
+      });
+
+      if (user) {
+        const boardName = `${user.name}의 Board`;
+        await this.boardsRepository.createBoard(
+          workspace.workspaceId,
+          boardName,
+        );
+      }
+    }
+
     return workspace;
-  };
+  }
 }
 
 export default WorkspacesRepository;
