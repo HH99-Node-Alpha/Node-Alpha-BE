0586a0405f162c1ec0426b9bbffa64e249143ead
jieunpark23 <jenne7700@gmail.com>
2023-09-27 12:17:39 +0900

    Feat: Kakao 로그인 구현
    - kakao 로그인을 하면 로그인이 되서
    프로필 정보를 가져오는거까지 구현
    - Token 발급하는건아직 진행안됨.
    추가된 항목
    passport/index.ts
    passport/kakaoStrategy.ts
    routes/login.ts
    
    - 스키마 User 테이블 변경

--- types/login.ts
+++ types/login.ts
@@ -8,3 +8,16 @@ export interface LoginRequest {
 export interface CustomExpressRequest extends Request {
   user?: any;
 }
+
+declare global {
+  namespace Express {
+  interface User {
+  userId: number;
+  name?: string;
+  password?: string | null;
+  profileUrl?: string | null;
+  kakaoLoggedInToken?: string | null;
+  googleLoggedInToken?: string | null;
+  }
+  }
+  }
